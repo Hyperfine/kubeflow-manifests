e35c21c3a83dc4bb7aa9f72d1d55bb0afc6819ce
Scott Young <syoung@hyperfine.io>
2022-02-18 11:18:35 -0800

    istio dag

--- terraform/stage_0/istio.tf
+++ terraform/stage_0/istio.tf
@@ -12,23 +12,27 @@ data "kustomization_build" "resources" {
 }
 
 resource "kustomization_resource" "istio_crds" {
+  depends_on = [kustomization_resource.cert]
   for_each = data.kustomization_build.istio_crds.ids
 
   manifest = data.kustomization_build.istio_crds.manifests[each.value]
 }
 
 resource "kustomization_resource" "istio_ns" {
+  depends_on = [kustomization_resource.istio_crds]
   for_each = data.kustomization_build.istio_ns.ids
 
   manifest = data.kustomization_build.istio_ns.manifests[each.value]
 }
 resource "kustomization_resource" "istio_install" {
+  depends_on = [kustomization_resource.istio_ns]
   for_each = data.kustomization_build.istio_install.ids
 
   manifest = data.kustomization_build.istio_install.manifests[each.value]
 }
 
 resource "kustomization_resource" "resources" {
+  depends_on = [kustomization_resource.istio_install]
   for_each = data.kustomization_build.resources.ids
 
   manifest = data.kustomization_build.resources.manifests[each.value]
