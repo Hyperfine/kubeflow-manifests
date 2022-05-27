0ae145d5425941c95fd00781e4e31dbd6eae2d6c
Scott Young <syoung@hyperfine.io>
2022-05-27 00:30:15 -0700

    readd kubectl

--- terraform/kf-base/main.tf
+++ terraform/kf-base/main.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+      kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
       kustomization = {
       source  = "kbst/kustomization"
       version = "0.8.1"
