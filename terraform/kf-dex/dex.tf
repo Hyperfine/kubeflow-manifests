c02119e807719d3f726ebb2972ad2d2a3a767312
Scott Young <syoung@hyperfine.io>
2022-06-07 10:53:10 -0700

    dex config

--- terraform/kf-dex/dex.tf
+++ terraform/kf-dex/dex.tf
@@ -2,12 +2,43 @@ data "kustomization_build" "oidc-auth" {
   path = "./../../common/oidc-authservice/base"
 }
 
-data "kustomization_build" "dex" {
-  path = "./../../common/dex/overlays/istio"
-}
-
 data "kustomization_build" "profiles" {
   path = "./../../apps/profiles/upstream/overlays/kubeflow"
+}
+
+
+resource "helm_release" "dex" {
+  repository = "https://charts.dexidp.io"
+  name       = "dex"
+  chart      = "dex"
+  version    = "0.8.3"
+  namespace  = "dex"
+  create_namespace = true
+  values = [<<YAML
+config:
+  storage:
+    type: memory
+  issuer: asdf
+
+  connectors:
+  - type: oidc
+    id: okta
+    name: Okta
+    config:
+      # Canonical URL of the provider, also used for configuration discovery.
+      # This value MUST match the value returned in the provider config discovery.
+      #
+      # See: https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderConfig
+      issuer: https://accounts.google.com
+
+      # Connector config values starting with a "$" will read from the environment.
+      clientID: $GOOGLE_CLIENT_ID
+      clientSecret: $GOOGLE_CLIENT_SECRET
+
+      # Dex's issuer URL + "/callback"
+      redirectURI: http://127.0.0.1:5556/callback
+YAML
+  ]
 }
 
 
@@ -17,12 +48,6 @@ resource "kustomization_resource" "oidc-auth" {
   manifest = data.kustomization_build.oidc-auth.manifests[each.value]
 }
 
-resource "kustomization_resource" "dex" {
-  depends_on = [kustomization_resource.oidc-auth]
-  for_each = data.kustomization_build.dex.ids
-
-  manifest = data.kustomization_build.dex.manifests[each.value]
-}
 
 
 resource "kustomization_resource" "profiles" {
--- terraform/kf-dex/test.tfvars
+++ terraform/kf-dex/test.tfvars
@@ -1,2 +1,3 @@
 aws_region="us-east-1"
 eks_cluster_name ="hyperfine-dev-eks-cluster-kf-dl"
+zone_id = "Z0808369SBLWX01BXVA7"
